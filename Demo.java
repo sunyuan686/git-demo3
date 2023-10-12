ff5339d4ee8e4a697db8446c60cbdd9c92b1530c
sunyuan <sunyuan@aspirecn.com>
2023-10-12 10:23:04 +0800

    feat(Demo类): Demo类新增method1和method2方法
    this long description
    
    BREAKING CHANGE: this Breaking changes
    
    Closes this closed issues

--- Demo.java
+++ Demo.java
@@ -1,14 +1,26 @@
 import java.util.ArrayList;
-import java.util.concurrent.TimeUnit;
 
 public class Demo {
+
     public static void main(String[] args) {
         System.out.println("HelloKitty");
         ArrayList<String> list = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1; i++) {
             list.add("Hello" + i);
         }
         list.forEach(item -> System.out.println(item));
 
+        methond1();
+
     }
+    public static void methond1(){
+        System.out.println("method1");
+    }
+
+
+
+    public static void methond2(){
+        System.out.println("method2");
+    }
+
 }
